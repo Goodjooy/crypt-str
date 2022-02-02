14378840bf197908a26bc50d2ae696609926cefb
Frozen String <964413011@qq.com>
2022-02-02 20:59:18 +0800

    :art: 为 `str_wraper`提供可能的错误处理

--- src/str_wraper.rs
+++ src/str_wraper.rs
@@ -1,11 +1,24 @@
 use std::borrow::Cow;
 
+#[derive(Debug)]
+pub struct NoError;
+
+impl std::fmt::Display for NoError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "NoError")
+    }
+}
+
+impl std::error::Error for NoError {}
+
 /// `CryptString` 内部的str容器
 pub trait StrWraper: Sized {
+    /// 当包装时，可能会产生错误，此处为错误关联类型
+    type Error: std::error::Error;
     /// 从容器中获取引用
     fn into_ref(&self) -> &str;
-    /// 从引用获取本体
-    fn from_owner(src: String) -> Self;
+    /// 从引用获取本体，错误时 返回`Err(Self::Error)`
+    fn from_owner(src: String) -> Result<Self, Self::Error>;
 }
 
 impl StrWraper for String {
@@ -13,9 +26,11 @@ impl StrWraper for String {
         self.as_str()
     }
 
-    fn from_owner(src: String) -> Self {
-        src
+    fn from_owner(src: String) -> Result<Self, Self::Error> {
+        Ok(src)
     }
+
+    type Error = NoError;
 }
 
 impl<'s> StrWraper for Cow<'s, str> {
@@ -23,8 +38,21 @@ impl<'s> StrWraper for Cow<'s, str> {
         &*self
     }
 
-    fn from_owner(src: String) -> Self {
-        Cow::Owned(src)
+    fn from_owner(src: String) -> Result<Self, Self::Error> {
+        Ok(Cow::Owned(src))
     }
+
+    type Error = NoError;
 }
 
+impl<'s> StrWraper for Box<String> {
+    type Error = NoError;
+
+    fn into_ref(&self) -> &str {
+        &self
+    }
+
+    fn from_owner(src: String) -> Result<Self, Self::Error> {
+        Ok(Box::new(src))
+    }
+}
